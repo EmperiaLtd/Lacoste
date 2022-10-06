79b65f775d8e922b1ad9fc28bd899a237142f839
Pushpendraasharma <pushpendra.sharma@dotsquares.com>
2022-10-06 14:49:43 +0530

    changed db and stack details

--- SAM-bloomingdales/Application/db.py
+++ SAM-bloomingdales/Application/db.py
@@ -1,10 +1,11 @@
 import redis
 
-db_endpoint = "redis-18296.c3.eu-west-1-2.ec2.cloud.redislabs.com"
-db_port = "18296"
+#db_endpoint = "redis-18296.c3.eu-west-1-2.ec2.cloud.redislabs.com"
+db_endpoint = "redis-15651.c81.us-east-1-2.ec2.cloud.redislabs.com"
+db_port = "15651"
 db_no = "0"
 db_user = "default"
-db_auth = "BzzCfwclDJJYf6kzx7ffwiInlZL5oxb5"
+db_auth = "dsnd7ACtTEN6GuNGGRhqyAdN3pLhXLQ3"
 
 db_backup_endpoint = "redis-15651.c81.us-east-1-2.ec2.cloud.redislabs.com"
 db_backup_port = "15651"
